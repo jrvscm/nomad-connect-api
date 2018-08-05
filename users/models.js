a68708dd6e60461c0705a5a3132194e420494740
Chris Jarvis <jrvscm@gmail.com>
2018-08-04 19:57:52 -0600

    update to es6

--- users/models.js
+++ users/models.js
@@ -30,11 +30,11 @@ UserSchema.methods.apiRepr = () => {
     };
 };
 
-UserSchema.methods.validatePassword = function(password) {
+UserSchema.methods.validatePassword = (password) => {
     return bcrypt.compare(password, this.password);
 };
 
-UserSchema.statics.hashPassword = function(password) {
+UserSchema.statics.hashPassword = (password) => {
     return bcrypt.hash(password, 10);
 };
 
